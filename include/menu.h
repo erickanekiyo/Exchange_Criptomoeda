7689d7dde127c4ecfe2f3fb90a7e7f72fe9ffdee
Eric.K <erickanekiyo@gmail.com>
2025-04-14 13:08:24 -0300

    adição das bibliotecas faltantes

--- include/menu.h
+++ include/menu.h
@@ -1,6 +1,11 @@
 #ifndef MENU_H
 #define MENU_H
 
+#include "tipos.h"
+#include "format_cpf.h"
+#include "consultar_saldo.h"
+#include "depositar_valor.h"
+
 void menu(struct Usuario* usuario, struct Carteira* carteira, struct Cotacoes* cotacoes, struct Taxas* taxas);
 
 #endif
